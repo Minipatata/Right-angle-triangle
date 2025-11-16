9ea2be866b90c096f1b9aded0327b3a90ed96988
Minipatata <ashleynaemmalee3827@ele.cssda.ca>
2025-11-16 12:12:47 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+#Write a program to demonstrate a right angle triangle pattern?
+number_stars=int(input("Please, enter any number that you like:"))
+for i in range(number_stars):
+        print("*"*i)
